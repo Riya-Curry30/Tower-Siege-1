a800d21cbae27af0aa2f0be8e7bbeb351a7da0d8
Riya-Curry30 <69163201+Riya-Curry30@users.noreply.github.com>
2020-12-31 13:37:59 -0800

    Add files via upload

---
+++ turquoiseBox.js
@@ -0,0 +1,32 @@
+class TurquoiseBox
+{
+    constructor(x,y)
+    {
+      var options =
+      {
+        'restitution':0.8,
+        'friction':0,
+        
+      }
+
+      this.body = Bodies.rectangle(x,y,30,40,options);
+      this.x = x;
+      this.y = y;
+
+      World.add(world,this.body);
+     
+    }
+
+    display()
+    {
+      var angle = this.body.angle;
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      pop();
+      var pos = this.body.position;
+      rectMode(CENTER);
+      fill("mediumturquoise");
+      rect(pos.x, pos.y, 30,40);
+    }
+}
